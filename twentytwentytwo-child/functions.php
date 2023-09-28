d13d07828be5d73cbe6ccf11f4c650015f2187e1
YiannisSofianos <91688312+YiannisSofianos@users.noreply.github.com>
2023-09-28 12:52:28 +0300

    child theme

---
+++ twentytwentytwo-child/functions.php
@@ -0,0 +1,11 @@
+<?php
+//
+//add_action('wp_enqueue_scripts', 'child_files');
+//
+//function child_files(): void
+//{
+//    if (get_post_type() == 'page') {
+//        wp_enqueue_style('child_theme_styles', get_theme_file_uri('/css/assignment.css'));
+//    }
+//}
+// I didn't use it. I added the css in page-assignment via a link tag in <head> element of assignment page
---
+++ twentytwentytwo-child/page-assignment.php
@@ -0,0 +1,68 @@
+<header>
+    <link rel="stylesheet" href="<?php echo get_theme_file_uri() ?>/css/assignment.css" type="text/css" media="screen"/>
+    <link href='https://fonts.googleapis.com/css?family=Montserrat' rel='stylesheet'>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</header>
+<body <?php body_class(); ?>>
+<div class="hero-banner">
+    <div class="book-button">
+        <a href="#">BOOK YOUR STAY</a>
+    </div>
+    <img class="hero-banner-back-image"
+         src="<?php echo get_theme_file_uri() . '/images/photo.png'?>">
+    <img class="hero-banner-back-image-mobile"
+         src="<?php echo get_theme_file_uri() . '/images/banner-image-mobile.png'?>">
+    <div class="center-container">
+        <div class="center-container-title">
+            <h1>Grand Opening</h1>
+        </div>
+        <div class="center-container-subtitle">
+            <a>We are in great anticipation of the moment when our doors open to welcome you.</a>
+        </div>
+    </div>
+    <div class="box-image-container">
+        <div class="box-image-div">
+            <img src="<?php echo get_theme_file_uri(). '/images/slh.svg'?>">
+        </div>
+    </div>
+</div>
+<div class="welcome-container">
+        <div class="right-column">
+            <div class="right-column-image">
+                <img src="<?php echo get_theme_file_uri() . '/images/welcome-image.png'?> ">
+            </div>
+            <div class="welcome-paragrapgh">
+                <div class="text-container">
+                    <h1>Welcome</h1>
+                    <p>Donec laoreet lacus quam, eget ornare dolor fringilla sed. Donec venenatis lacinia neque, quis vehicula sem varius in. Nulla scelerisque neque magna, sed mollis mi accumsan et. Vestibulum porta rutrum justo vitae molestie. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Mauris tempor sagittis ex, a tincidunt est iaculis eget. Nunc in semper ex.</p>
+                </div>
+            </div>
+        </div>
+</div>
+<div class="new-era-container">
+    <div class="ne-left-column">
+        <div class="header">
+            <h3>We enter a new era…</h3>
+        </div>
+        <div class="content"><p>Drawing inspiration from all the countries of the Mediterranean basin, MINOIS tells a riveting story of harmonious contrasts.The story begins with the iconic white, cubist houses that are a tradition in Cycladic architecture. Typically clustered together, they represent a blank Cycladic canvas of endless possibilities, waiting to be drawn upon. Their emblematic whiteness, that radiates a light so pure, is warmed by lively colors and natural textures, as reflections of the Spanish, French and Italian rivieras with touches of Moroccan decor, all of which cumulatively personify the aura of the Mediterranean, as expressions of a singular, more inclusive identity.</p></div>
+    </div>
+    <div class="ne-right-column">
+        <div class="rhombus"></div>
+        <div class="pool-image-container">
+            <img class="pool-image" src="<?php echo get_theme_file_uri() . '/images/pool-342x512.png'?>">
+        </div>
+        <div class="lounge-image-container">
+            <img class="lounge-image" src="<?php echo get_theme_file_uri() . '/images/lounge.png' ?>">
+        </div>
+        <div class="fireplace-image-container">
+            <img class="fireplace-image" src="<?php echo get_theme_file_uri() . '/images/fireplace.png'?>">
+        </div>
+    </div>
+</div>
+<div class="grant-opening">
+    <div class="inner-container">
+        <div class="title"><h1>Grand opening</h1></div>
+        <div class="subtitle"><p>We are in great anticipation of the moment when our doors open to welcome you.</p></div>
+        <a href="#">BOOK YOUR STAY</a>
+    </div>
+</div>
